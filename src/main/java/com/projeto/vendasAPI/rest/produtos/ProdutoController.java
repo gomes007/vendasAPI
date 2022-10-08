9b318569716ba1401c48e9a0e05e7d5340b86f35
Paulo Gomes <gomes7.paulo@gmail.com>
2022-10-07 21:57:47 -0300

    api

--- src/main/java/com/projeto/vendasAPI/rest/produtos/ProdutoController.java
+++ src/main/java/com/projeto/vendasAPI/rest/produtos/ProdutoController.java
@@ -3,13 +3,11 @@ package com.projeto.vendasAPI.rest.produtos;
 import com.projeto.vendasAPI.model.Produto;
 import com.projeto.vendasAPI.model.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/produtos")
+@CrossOrigin("*")
 public class ProdutoController {
 
 
